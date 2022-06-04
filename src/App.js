0ca268ecf208c1506218848756ae8217b10d6d16
nguye3hg <88401567+nguye3hg@users.noreply.github.com>
2022-06-04 15:26:24 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('Hoang');
+  function onInputChange(event){   
+    setName(event.target.value);
+  }
+  function onButtonSubmit(){
+    // fetch('https://ornate-tartufo-20c28a.netlify.app/api/Users',{
+    //   method:'get',
+    //   headers: {'Content-type':'application/json'},
+    //   body: JSON.stringify({
+    //     name: name
+    //   })
+    // })
+    // console.log(name);
+    fetch('https://ornate-tartufo-20c28a.netlify.app/api/Users')
+        .then(response => response.json())
+        .then(data => setName(data));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <input onChange={(e)=>onInputChange(e)}/>
+      <button onClick={onButtonSubmit}/>
     </div>
   );
 }
